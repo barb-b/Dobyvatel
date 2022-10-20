7e6a4f14287f1e4ed048445cc823d176057fd872
barb-b <barbora.jurkovcova@gmail.com>
2022-10-20 07:17:55 +0200

    excel sa nacita a radio button sa spustia ako random otazky

--- app/src/main/java/com/example/dobyvatel/Quizz.kt
+++ app/src/main/java/com/example/dobyvatel/Quizz.kt
@@ -1,23 +1,30 @@
 package com.example.dobyvatel
 
-import android.content.ContentProvider
-import android.content.res.AssetFileDescriptor
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.RadioButton
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContentProviderCompat.requireContext
 import com.example.dobyvatel.databinding.ActivityQuizzBinding
+import com.example.dobyvatel.objects.MilkyWayPlanets
 import com.example.dobyvatel.objects.QuizzClass
-import org.apache.poi.ss.usermodel.WorkbookFactory
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
-import java.io.BufferedInputStream
-import java.io.File
-import java.io.FileInputStream
-import java.io.InputStream
+import kotlin.collections.ArrayList
+import kotlin.random.Random
 
 
 class Quizz : AppCompatActivity() {
 
     private lateinit var binding: ActivityQuizzBinding
+
+    var row = 0
+    val question = 1
+    val answer1 = 2
+    val answer2 = 3
+    val rightAnswer = 4
+    var currentPlanet = "SLNKO"
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,46 +32,105 @@ class Quizz : AppCompatActivity() {
         binding = ActivityQuizzBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        var listOfQuestion = ArrayList<QuizzClass>()
+//        whatIsCurrentPlanet()
 
+        var listOfQuestion = ArrayList<QuizzClass>()
 
         ///Otvor excel
         val open = assets.open("planety_quizz.xlsx")
         val workBook = XSSFWorkbook(open)
         val sheet = workBook.getSheetAt(0)
-
-
-        val name = sheet.getRow(0).getCell(0)
         val rowIterator = sheet.rowIterator()
 
-        var row = 0
-        val otazka = 1
-        val odp = 2
-        val col2 = 3
-        val correctcolu = 4
 
-        while(rowIterator.hasNext()){
+        ///Napln array otazkami o sucastnej planete
+        while (rowIterator.hasNext()) {
 
-            if(sheet.getRow(row).getCell(0).toString() == "SLNKO"){
+            if (sheet.getRow(row).getCell(0).toString() == currentPlanet) {
 
-                val otazka = QuizzClass(sheet.getRow(row).getCell(otazka).toString(),
-                    sheet.getRow(row).getCell(odp).toString(),
-                    sheet.getRow(row).getCell(col2).toString(),
-                    sheet.getRow(row).getCell(correctcolu).toString())
+                val otazka = QuizzClass(
+                    sheet.getRow(row).getCell(question).toString(),
+                    sheet.getRow(row).getCell(answer1).toString(),
+                    sheet.getRow(row).getCell(answer2).toString(),
+                    sheet.getRow(row).getCell(rightAnswer).toString()
+                )
                 listOfQuestion.add(otazka)
                 row++
             }
             rowIterator.next()
         }
 
-        println()
-        
+        var randomOrderRadio = ArrayList<RadioButton>()
+        randomOrderRadio.add(binding.radioButton2)
+        randomOrderRadio.add(binding.radioButton3)
+        randomOrderRadio.add(binding.radioButton4)
 
 
-        binding.textView2.text = name.toString()
+        ///TODO najprv random cislo -> potom vytiahnut z listu to
+        createRandomQuestion(listOfQuestion, randomOrderRadio)
+
+//        val randomNu = Random.nextInt(0, listOfQuestion.size)
+//        val objectQue = listOfQuestion.get(randomNu)
+//        binding.radioButton2.text = objectQue.answer1
+//        binding.radioButton3.text = objectQue.answer2
+//        binding.radioButton4.text = objectQue.correctAns
+//        randomOrderRadio.shuffle()
+//        binding.radioGroup.removeAllViews()
+//        for (radio in randomOrderRadio) {
+//            binding.radioGroup.addView(radio)
+//        }
+
+
+
+        binding.button.setOnClickListener {
+            val radio: RadioButton = findViewById(binding.radioGroup.checkedRadioButtonId)
+            binding.textView2.text = radio.text
+            createRandomQuestion(listOfQuestion, randomOrderRadio)
+        }
+
+
     }
 
+    fun whatIsCurrentPlanet() {
+        if (MilkyWayPlanets.sunIsPlaying) {
+            currentPlanet = "SLNKO"
+        } else if (MilkyWayPlanets.mercuryIsPlaying) {
+            currentPlanet = "MERKUR"
+        } else if (MilkyWayPlanets.venusIsPlaying) {
+            currentPlanet = "VENUSA"
+        } else if (MilkyWayPlanets.earthIsPlaying) {
+            currentPlanet = "ZEM"
+        } else if (MilkyWayPlanets.marsIsPlaying) {
+            currentPlanet = "MARS"
+        } else if (MilkyWayPlanets.jupiterIsPlaying) {
+            currentPlanet = "JUPITER"
+        } else if (MilkyWayPlanets.saturnIsPlaying) {
+            currentPlanet = "SATURN"
+        } else if (MilkyWayPlanets.uranusIsPlaying) {
+            currentPlanet = "URAN"
+        } else if (MilkyWayPlanets.neptuneIsPlaying) {
+            currentPlanet = "NEPTUN"
+        }
+    }
 
+    fun createRandomQuestion(list: ArrayList<QuizzClass>, radioButton:ArrayList<RadioButton>){
+        val randomNu = Random.nextInt(0, list.size)
+        val objectQuestion = list.get(randomNu)
+
+        binding.radioGroup.clearCheck()
+        binding.radioGroup.removeAllViews()
+
+        binding.radioButton2.text = objectQuestion.answer1
+        binding.radioButton3.text = objectQuestion.answer2
+        binding.radioButton4.text = objectQuestion.correctAns
+
+        radioButton.shuffle()
+
+        for (radio in radioButton) {
+            binding.radioGroup.addView(radio)
+        }
+
+    }
 
 
 }
--- app/src/main/java/com/example/dobyvatel/objects/QuizzClass.kt
+++ app/src/main/java/com/example/dobyvatel/objects/QuizzClass.kt
@@ -2,15 +2,15 @@ package com.example.dobyvatel.objects
 
 class QuizzClass {
 
-    val otazka: String
-    val odpoved1: String
-    val odpoved2: String
-    val spravnaodpoved: String
+    val question: String
+    val answer1: String
+    val answer2: String
+    val correctAns: String
 
-    constructor(otazka: String, odpoved1: String, odpoved2: String, spravnaodpoved: String) {
-        this.otazka = otazka
-        this.odpoved1 = odpoved1
-        this.odpoved2 = odpoved2
-        this.spravnaodpoved = spravnaodpoved
+    constructor(question: String, answer1: String, answer2: String, correctAns: String) {
+        this.question = question
+        this.answer1 = answer1
+        this.answer2 = answer2
+        this.correctAns = correctAns
     }
 }
